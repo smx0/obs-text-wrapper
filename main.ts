10956f16202103b43be17dca9876b010e5073a19
smx0 <98167565+smx0@users.noreply.github.com>
2023-01-17 14:30:25 -0600

    working version

--- main.ts
+++ main.ts
@@ -1,81 +1,79 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface TextWrapSettings {
+	quickTagOne: string;
+	quickTagTwo: string;
+	quickTagThree: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: TextWrapSettings = {
+	quickTagOne: "",
+	quickTagTwo: "",
+	quickTagThree: ""
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TextWrap extends Plugin {
+	settings: TextWrapSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// =============================
+		// commands for command palette
+		// =============================
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'wrap-text',
+			name: 'Enter new tags',
+
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+				const selection = editor.getSelection();
+
+				const tagAndText = (tag: string, text: string) => {
+					editor.replaceSelection(`<${tag}>${text}</${tag}>`);
+				};
+
+				new TextWrapModal(this.app, selection, tagAndText).open()
+			},
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: 'quick-tag-one',
+			name: 'Quick Tag One',
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				const selection = editor.getSelection();
+				const tag = this.settings.quickTagOne;
+				editor.replaceSelection(`<${tag}>${selection}</${tag}>`)
+			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.addCommand({
+			id: 'quick-tag-two',
+			name: 'Quick Tag Two',
+
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				const selection = editor.getSelection();
+				const tag = this.settings.quickTagTwo;
+				editor.replaceSelection(`<${tag}>${selection}</${tag}>`)
+			},
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		this.addCommand({
+			id: 'quick-tag-three',
+			name: 'Quick Tag Three',
+
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				const selection = editor.getSelection();
+				const tag = this.settings.quickTagThree;
+				editor.replaceSelection(`<${tag}>${selection}</${tag}>`)
+			},
+		});
+
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,47 +89,100 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class TextWrapModal extends Modal {
+	modalTag: string;
+	modalText: string;
+
+	tagAndText: (modalTag: string, modalText: string) => void;
+
+	constructor(
+		app: App,
+		defaultText: string,
+		tagAndText: (modalTag: string, modalText: string) => void
+	) {
 		super(app);
+		this.modalText = defaultText;
+		this.tagAndText = tagAndText;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+
+		contentEl.createEl("h3", { text: "Enter tags" })
+
+		new Setting(contentEl)
+			.setName("Press submit button to send tags")
+			.addText((text) =>
+				text.onChange((value) => {
+					this.modalTag = value;
+				})
+			);
+
+
+		new Setting(contentEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText("Submit")
+					.setCta()
+					.onClick(() => {
+						this.tagAndText(this.modalTag, this.modalText)
+						this.close();
+					}));
 	}
 
+
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SettingTab extends PluginSettingTab {
+	plugin: TextWrap;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TextWrap) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Customize Quick Tags' });
+		containerEl.createEl('p', { text: 'Tags will be applied to selected text as: <tagName>selectedText</tagName>' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Quick Tag One')
+			.setDesc('Customize')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.quickTagOne)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.quickTagOne = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('Quick Tag Two')
+			.setDesc('Customize')
+			.addText(text => text
+				.setValue(this.plugin.settings.quickTagTwo)
+				.onChange(async (value) => {
+					this.plugin.settings.quickTagTwo = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Quick Tag Three')
+			.setDesc('Customize')
+			.addText(text => text
+				.setValue(this.plugin.settings.quickTagThree)
+				.onChange(async (value) => {
+					this.plugin.settings.quickTagThree = value;
+					await this.plugin.saveSettings();
+				}));
+
+
 	}
 }
